a6ed140e42664241a3c632231a6e1095b4c8a8c5
PAYAL0987 <payalkumari9910@gmail.com>
2024-12-24 14:09:09 +0530

    added file

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,20 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MyComponent  from './myComponent/MyComponent'
-import Header from './Header/header'
-
 
 function App() {
- 
+ // const [count, setCount] = useState(0)
 
   return (
     <>
-    <Header />
-    <MyComponent />
-
+     
+        <header />
+        <cards />
     </>
   )
 }
 
-export default App
+export default App;
